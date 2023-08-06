5303a5a5dbd2c21d57e87b71a70a30fc9992ec95
beingsie <ada@beingsie.com>
2023-08-05 23:03:50 -0700

    feat: Add sorting_hat.py)

---
+++ sorting_hat.py
@@ -0,0 +1,32 @@
+# Houses
+gryffindor = int(0)
+ravenclaw = int(0)
+huhfflepuff = int(0)
+slytherin = int(0)
+
+# Ask user for favored house
+user_house = input('What house would you want to be in?')
+# Print choice question pool 1
+print("🦁 Gryffindor")
+print("🦅 Ravenclaw")
+print("🦡 Hufflepuff")
+print("🐍 Slytherin")
+
+if user_house == int(1) or str("gryffindor"):
+    gryffindor += 1
+    ravenclaw += 1
+    # Debug
+    print("🦁 Gryffindor |" + gryffindor)
+
+# Assign favored user house
+if user_house == str("gryffindor") and int(1):
+    
+    # Create message for user house
+    msg_user_house = str(f"User House: ", user_house)
+
+
+if user_house == int(1):
+    user_house += int(1)
+    print(msg_user_house)
+else:
+    print(msg_user_house)
