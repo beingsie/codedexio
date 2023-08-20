6a5b9660c2f6869141dc915467a4fab5e87e98c2
beingsie <ada@beingsie.com>
2023-08-20 12:10:38 -0700

    Feat: Add snapple_facts.py

---
+++ challenge_pack/control_flow/snapple_facts.py
@@ -0,0 +1,18 @@
+# Import random function
+import random
+
+# Store random generated number from 1-5
+num = random.randint(1, 5)
+
+if num == 1:
+    print("Flamingos turn pink from eating shrimp.")
+elif num == 2:
+    print("The only food that doesn’t spoil is honey.")
+elif num == 3:
+    print("Shrimp can only swim backwards.")
+elif num == 4:
+    print("The lifespan of a taste bud is about 10 days.")
+elif num == 5:
+    print("It is impossible to sneeze while sleeping.")
+else:
+    "Error"
