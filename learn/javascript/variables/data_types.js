2cdf0316f0ba5685ee5d7fe4a3e94f659651f2f5
Beingsie <ada@beingsie.com>
2023-10-13 07:33:04 -0700

    feat: add basic data types and organize code

--- learn/javascript/variables/data_types.js
+++ learn/javascript/variables/data_types.js
@@ -1,7 +1,16 @@
+// Most common starting data types
+const userName = "Sergio"; // String
+let userAge = 30; // Number
+let isAwesome = true; // Boolean
+let isEngineer = undefined; // Undefined
+let isEmpty = null; //
+
+// Codedex exercise
 const h2o = "Water";
 let favoriteDrink = h2o;
 const isHydrated = true;
 
+// Print to console
 console.log(h2o)
 console.log(favoriteDrink)
 console.log(isHydrated)
