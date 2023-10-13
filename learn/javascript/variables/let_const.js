92cf0e7659d28c2e331f4d7451f781195eb8a712
Beingsie <ada@beingsie.com>
2023-10-13 01:36:26 -0700

    feat: add learning path javascript

---
+++ learn/javascript/variables/let_const.js
@@ -0,0 +1,19 @@
+// Define variables
+let userTwitterName = "Beingsie";
+let userAge = 29;
+const userBirthday = "05/30/1993";
+const userGender = "Male";
+let userGithubUrl = "https://github.com/beingsie";
+
+// Print variables
+console.log(userTwitterName)
+console.log(userAge)
+console.log(userBirthday)
+console.log(userGender)
+console.log(userGithubUrl)
+
+// Reassign new value to user age
+userAge = 30;
+
+// Print new value
+console.log(userAge)
