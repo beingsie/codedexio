964d2eedb53fd0a9af508cfefee4d0d0e02a4428
Beingsie <ada@beingsie.com>
2023-10-14 15:46:24 -0700

    feat: add good_afternoon.js

---
+++ learn/javascript/variables/good_afternoon.js
@@ -0,0 +1,10 @@
+// Set current hour
+hour = 3.31;
+
+if (hour < 12) {
+    // Display good morning message if TRUE
+    console.log("Good morning 🌞")
+} else {
+    // Display good afternoon message if FALSE
+    console.log("Good afternoon ☁️")
+}
