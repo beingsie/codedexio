89456dca818b00153577a3702e3d96aa3571c156
Beingsie <ada@beingsie.com>
2023-10-14 09:00:23 -0700

    feat: add coin_flip.js

---
+++ learn/javascript/variables/coin_flip.js
@@ -0,0 +1,14 @@
+// Display a message indicating coin flipping
+console.log("Flipping coin.. 🪙");
+
+// Generate a random number between 0 and 1
+let num = Math.random();
+
+// Check if the random number is greater than 0.5 (a 50% chance)
+if (num > 0.5) {
+    // If the condition is met, it's Heads
+    console.log("It's HEADS");
+} else {
+    // If not, it's Tails
+    console.log("It's TAILS");
+}
