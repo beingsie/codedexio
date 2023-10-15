2b28d9b33873d7d2c677914d1669f81736afadcc
Beingsie <ada@beingsie.com>
2023-10-14 19:39:03 -0700

    feat: add magic8.js

---
+++ learn/javascript/variables/magic8.js
@@ -0,0 +1,33 @@
+// User question
+let userQuestion = "Can I learn JavaScript fundamentals in a week?";
+// Randomize a number between 0 to 9
+let magic8Random = Math.floor(Math.random() * 10);
+
+if (magic8Random == 1) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Yes - definitely.");
+} else if (magic8Random == 2) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: It is decidedly so.");
+} else if (magic8Random == 3) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Without a doubt.");
+} else if (magic8Random == 4) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Reply hazy, try again.");
+} else if (magic8Random == 5) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Ask again later.");
+} else if (magic8Random == 6) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Better not tell you now.");
+} else if (magic8Random == 7) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: My sources say no.");
+} else if (magic8Random == 8) {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Outlook not so good.");
+} else {
+    console.log("Question: " + userQuestion);
+    console.log("Answer: Very doubtful.");
+}
