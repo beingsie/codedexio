170287f22d33b9b02aa57a5ec5101ba79a6e8d09
Beingsie <ada@beingsie.com>
2023-11-12 20:22:52 -0800

    feat: add variables.py

---
+++ courses/python/02_variables/variables.py
@@ -0,0 +1,15 @@
+# Tiny program using all the variables in this lesson
+# Using each variable aleast twice
+
+# Set variables
+username = 'Beingsie'
+rank = 'Silver'
+python_progress = 0.54
+javascript_progress = 0.44
+level = 7
+total_xp = 1590
+club_member = True
+inactive = False
+
+# Print variables
+print(username, rank, python_progress, javascript_progress, level, total_xp, club_member, inactive)
