bd709ed3abb9a4521ee2c131a5584c4bc473eb8d
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2023-11-13 14:57:01 -0800

    docs: update enrollment, membership status variables & logic

--- personal_exercises/python/90-day-free-fares.py
+++ personal_exercises/python/90-day-free-fares.py
@@ -3,34 +3,37 @@
 # Import random module
 import random
 
-# Confirm if user has already enrolled before
-enrolled = input("Have you enrolled before? (Y/N): ")
+# Initialize membership status
+active_membership = 0
+
+# Confirm if user has already enrollment_status before
+enrollment_status = input("Have you enrollment_status before? (Y/N): ")
 
 if (
-    enrolled == "y"
-    or enrolled == "Y"
-    or enrolled == "yes"
-    or enrolled == "YES"
-    or enrolled == "Yes"
-    or enrolled == "yEs"
-    or enrolled == "yeS"
+    enrollment_status == "y"
+    or enrollment_status == "Y"
+    or enrollment_status == "yes"
+    or enrollment_status == "YES"
+    or enrollment_status == "Yes"
+    or enrollment_status == "yEs"
+    or enrollment_status == "yeS"
 ):
-    is_enrolled = True
+    active_membership = True
     print(
         "Unfortunately you don't qualify for the 90 Day Free Fares as you're currently already an active customer. You can still qualify for the 20 free monthly fares."
     )
 elif (
-    enrolled == "n"
-    or enrolled == "N"
-    or enrolled == "no"
-    or enrolled == "NO"
-    or enrolled == "No"
-    or enrolled == "nO"
+    enrollment_status == "n"
+    or enrollment_status == "N"
+    or enrollment_status == "no"
+    or enrollment_status == "NO"
+    or enrollment_status == "No"
+    or enrollment_status == "nO"
 ):
-    is_enrolled = False
+   active_membership = False
     print("Great, you qualify for the 90 Day Free Fares Program!")
 else:
-    enrolled = input("Please enter (Y/N): ")
+    enrollment_status = input("Please enter (Y/N): ")
 
 # Ask user for details
 first_name = input("First Name: ")
@@ -45,7 +48,6 @@ email_address = input("Email Address: ")
 birth_date = input("Birth Date: ")
 gender = input("Gender: ")
 ethnicity = input("Ethnicity: ")
-is_enrolled = 0
 
 # Create confirmation number
 confirmation_number_id_identifier = "0000-"
@@ -53,7 +55,11 @@ confirmation_number = random.randint(100000, 999999)
 confirmation_num = confirmation_number_id_identifier + str(confirmation_number)
 
 # Print confirmation message
-print(
-    "You have successfully enrolled into the 90 Day Free Fares Program! Your confirmation number is "
-    + confirmation_num
-)
+if active_membership == True:
+	print('You have successfully enrolled into the 90 Day Free Fares Program!')
+	print('Your confirmation number is', confirmation_num)
+elif active_membership == False:
+	print('You have successfully enrolled into the monthly 20 Free Fares Program!')
+	print('Your confirmation number is', confirmation_num)
+else:
+	print('We're sorry, an error occured while processing your application.')
