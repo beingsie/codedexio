1f4e235e8652a24d851ac747e83e9d956bfedf4c
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2023-11-13 09:28:11 -0800

    refactor: update magic8.py for beginner friendly solution

--- courses/python/03_control_flow/magic8.py
+++ courses/python/03_control_flow/magic8.py
@@ -1,23 +1,28 @@
 # Import random module
 import random
 
-# Create list of random answers
-answer_list = [
-    # Random answers
-    "Yes - definitely.",
-    "It is decidedly so.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful."
-]
+# Ask user for a question
+question = input('Ask a question: ')
 
-# Ask user to pick a question
-user_question = str(input("What is your question? "))
+# Generate random number
+random_num = random.randint(1, 9)
 
-# Print random generated answer
-print(random.choice(answer_list))
+# Logic
+if random_num == 1:
+  print('Magic 8 Ball: Yes - definitely.')
+elif random_num == 2:
+  print('Magic 8 Ball: It is decidedly so.')
+elif random_num == 3:
+  print('Magic 8 Ball: Without a doubt.')
+elif random_num == 4:
+  print('Magic 8 Ball: Reply hazy, try again.')
+elif random_num == 5:
+  print('Magic 8 Ball: Ask again later.')
+elif random_num == 6:
+  print('Magic 8 Ball: Better not tell you now.')
+elif random_num == 7:
+  print('Magic 8 Ball: My sources say no.')
+elif random_num == 8:
+  print('Magic 8 Ball: Outlook not so good.')
+else:
+  print('Magic 8 Ball: Very doubtful.')
