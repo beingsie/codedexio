a051315f0a621a5a7b0046d113dc70bf831aee83
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2023-11-16 09:04:23 -0800

    fix: spelling of possessed

--- courses/python/terminal_game.py
+++ courses/python/terminal_game.py
@@ -1,5 +1,5 @@
 # Characters
-possed_bear = 'Possessed Bear' # Temp name
+possessed_bear = 'Possessed Bear' # Temp name
 punching_baby = 'Punching Baby' # Temp name
 
 # Universal Stats
@@ -7,8 +7,8 @@ xp = 0
 
 # - Character Stats
 # Possessed Bear
-possed_bear_hp = 250
-possed_bear_atk = 3
+possessed_bear_hp = 250
+possessed_bear_atk = 3
 
 # Punching Baby
 punching_baby_hp = 90
@@ -28,9 +28,9 @@ print(f'Welcome to Monster Royale {player_name.capitalize()}!')
 print(f'''
 [Characters]
 
-> {possed_bear}
- - HP: {possed_bear_hp}
- - Attack Power: {possed_bear_atk}
+> {possessed_bear}
+ - HP: {possessed_bear_hp}
+ - Attack Power: {possessed_bear_atk}
  
 > {punching_baby}
  - HP {punching_baby_hp}
@@ -42,7 +42,7 @@ selected_character = input('Choose a character: ').lower()
 
 if selected_character == punching_baby.lower():
 	print(f'Woah, you selected {punching_baby}!')
-elif selected_character == possed_bear.lower():
-	print(f'Woah, you selected {possed_bear}!')
+elif selected_character == possessed_bear.lower():
+	print(f'Woah, you selected {possessed_bear}!')
 else:
 	print(f'Pssst, you didn\'t select a character. Not putting up with this, buh-BYE.')
