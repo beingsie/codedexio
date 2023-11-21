f6e2c6a839c0516e9dc29a434ab555276135f58b
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2023-11-21 15:39:04 -0800

    refactor: add empty print lines for initial ui changes

--- courses/python/checkpoint/terminal_game.py
+++ courses/python/checkpoint/terminal_game.py
@@ -23,30 +23,38 @@ print("")
 
 # Welcome message
 print(f"Welcome to Randit Battle Royale {player_name.capitalize()}!")
+print("") # Empty string line - UI
 
 # - Character Selection
 # Display list of characters
 print(f"[Characters]")
 print(f"> 1. {punching_baby.title()}")
+print("") # Empty string line - UI
 print(f"- HP: {punching_baby_hp}")
 print(f"- Attack Power: {punching_baby_atk}")
+print("") # Empty string line - UI
 print(f"> 2. {possessed_bear.title()}")
 print(f"- HP: {possessed_bear_hp}")
 print(f"- Attack Power: {possessed_bear_atk}")
+print("") # Empty string line - UI
 
 # Character Selection
 selected_character = input("Choose a character: ").lower()
+print("") # Empty string line - UI
 
 # Loop user to select an available character if no available character is selected
 while (selected_character != punching_baby and selected_character != "1") and (selected_character != possessed_bear and selected_character != "2"):
-     selected_character = input("Please select an available character: ").lower()
+    selected_character = input("Please select an available character: ").lower()
+	print("") # Empty string line - UI
 
 if selected_character == punching_baby.lower() or selected_character == "1":
     selected_character = punching_baby
     print(f"Woah, you selected {punching_baby.title()}!")
+	print("") # Empty string line - UI
 elif selected_character == possessed_bear.lower() or selected_character == "2":
     selected_character = possessed_bear
     print(f"Woah, you selected {possessed_bear.title()}!")
+	print("") # Empty string line - UI
 else:
     print(f"Pssst, you didn't select a character. Not putting up with this, buh-BYE.")
 
@@ -54,6 +62,7 @@ else:
 
 # Simulated attack to player
 print(f"SIMULATING ATTACK TO {player_name.capitalize()}!")
+print("") # Empty string line - UI
 
 # Attack damage randomizer per character
 hit_possessed_bear = random.randint(1, possessed_bear_atk)
@@ -63,26 +72,33 @@ hit_punching_baby = random.randint(1, punching_baby_atk)
 if selected_character == punching_baby:
     update_punching_baby_hp = punching_baby_hp - hit_punching_baby
     print(f"{punching_baby.title()} has been hit with {hit_punching_baby} attack points!")
-    print(f"  HP: {update_punching_baby_hp}/{punching_baby_hp}")
+    print(f"> HP: {update_punching_baby_hp}/{punching_baby_hp}")
+	print("") # Empty string line - UI
     
 elif selected_character == possessed_bear:
     update_possessed_bear_hp = possessed_bear_hp - hit_possessed_bear
     print(f"{possessed_bear.title()} has been hit with {hit_possessed_bear} attack points!")
-    print(f"  HP: {update_possessed_bear_hp}/{possessed_bear_hp}")
+    print(f"> HP: {update_possessed_bear_hp}/{possessed_bear_hp}")
+	print("") # Empty string line - UI
 
 # Counter Attack - Player
 # Message for player
 print(f"Time for a counter attack {player_name.title()}!")
+print("") # Empty string line - UI
 
 # Attacking options
 print(f"{player_name.title()} select your next move!")
+print("") # Empty string line - UI
 print("> 1. Attack")
 print("> 2. Run Away")
+print("") # Empty string line - UI
 
 next_move = input(f"Please select your next move: ").lower()
+print("") # Empty string line - UI
 
 while (next_move != "1" and next_move != "attack") and (next_move != "2" and next_move != "run away"):
 	next_move = input("Please select a next move: ").lower()
+	print("") # Empty string line - UI
 
 if next_move == "1" or next_move == "attack":
 	if selected_character == punching_baby:
@@ -96,9 +112,15 @@ if next_move == "1" or next_move == "attack":
 elif next_move == "2" or next_move == "run away":
 	if selected_character == punching_baby:
 		xp = xp - 7
-		print(f"{punching_baby.title()} ran away from {possessed_bear.title()}! {player_name.title()} has been deducted {xp}XP for this action!")
+		print(f"{punching_baby.title()} ran away from {possessed_bear.title()}!")
+		print("") # Empty string line - UI
+		print(f"{player_name.title()} has been deducted {xp}XP for this action!")
+		print("") # Empty string line - UI
 		print(f"{player_name.title()}'s XP: {xp}")
 	elif selected_character == possessed_bear:
 		xp = xp - 7
-		print(f"{possessed_bear.title()} ran away from {punching_baby.title()}! {player_name.title()} has been deducted {xp}XP for this action!")
+		print(f"{possessed_bear.title()} ran away from {punching_baby.title()}!")
+		print("") # Empty string line - UI
+		print(f"{player_name.title()} has been deducted {xp}XP for this action!")
+		print("") # Empty string line - UI
 		print(f"{player_name.title()}'s XP: {xp}")
