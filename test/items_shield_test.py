b69dc4987fefcc70090d550c2414862a5724680c
Beingsie <ada@beingsie.com>
2023-12-03 13:26:42 -0800

    feat: add text color for rarity messages

--- test/items_shield_test.py
+++ test/items_shield_test.py
@@ -3,6 +3,16 @@
 # Import random module function randint
 from random import randint
 
+# Text color & reset
+RED = "\033[91m"
+GREEN = "\033[92m"
+BLUE = "\033[94m"
+CYAN = "\033[36m"
+MAGENTA = "\033[95m"
+MUSTARD_YELLOW = "\033[93m"
+GRAY = "\033[90m"
+RESET = "\033[0m"  # Reset text color to default
+
 # Character hp
 hp = 150
 
@@ -20,23 +30,35 @@ for x in range(35):
     # Loot rarity
     if trigger_lootdrop >= 1 and trigger_lootdrop <= 10:
         # Add shield boost to current hp
-        hp_shielded = hp + shield_common
-        print(hp_shielded)
+        hp_shielded = hp + shield_uncommon
+        print(f"Lootdrop gave {GRAY}Uncommon Shield{RESET}!")
+        print(f"{GREEN}Gained +{shield_uncommon}HP{RESET}")
+        print(f"{hp}/{hp_shielded}")
+        print()
 
     elif trigger_lootdrop >= 11 and trigger_lootdrop <= 15:
         # Add shield boost to current hp
         hp_shielded = hp + shield_rare
-        print(hp_shielded)
+        print(f"Lootdrop gave {MAGENTA}Rare Shield{RESET}!")
+        print(f"{GREEN}Gained +{shield_rare}HP{RESET}")
+        print(f"{hp}/{hp_shielded}")
+        print()
 
     elif trigger_lootdrop >= 16 and trigger_lootdrop <= 18:
         # Add shield boost to current hp
         hp_shielded = hp + shield_epic
-        print(hp_shielded)
+        print(f"Lootdrop gave {CYAN}Epic Shield{RESET}!")
+        print(f"{GREEN}Gained +{shield_epic}HP{RESET}")
+        print(f"{hp}/{hp_shielded}")
+        print()
 
     elif trigger_lootdrop >= 19 and trigger_lootdrop <= 20:
         # Add shield boost to current hp
         hp_shielded = hp + shield_legendary
-        print(hp_shielded)
+        print(f"Lootdrop gave {MUSTARD_YELLOW}Legendary Shield{RESET}!")
+        print(f"{GREEN}Gained +{shield_legendary}HP{RESET}")
+        print(f"{hp}/{hp_shielded}")
+        print()
 
     else:
         print(f"No luck, empty drop! [{trigger_lootdrop}]")
