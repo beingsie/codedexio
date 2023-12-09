6b8a02572b4e577170b9c6d39ad9d59301e8d568
Beingsie <ada@beingsie.com>
2023-12-08 20:47:24 -0800

    feat: add text_stagger.py

---
+++ test/text_stagger.py
@@ -0,0 +1,35 @@
+import time
+
+
+print()
+print("  Booting.")
+time.sleep(1)
+print("  Booting..")
+time.sleep(1)
+print("  Booting...")
+time.sleep(0.5)
+print()
+print("  ░▒▓  PLAYER REGISTRATION  ▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+time.sleep(0.25)
+print("""
+
+""")
+time.sleep(0.25)
+
+# Player name
+player_name = input("     ♢ Enter a Username > ")
+
+print()
+time.sleep(0.25)
+print("  Registering")
+time.sleep(0.5)
+print("  ↓  ")
+time.sleep(0.5)
+print("  ↓  ")
+time.sleep(0.5)
+print(f"  Welcome {player_name.title()}")
+time.sleep(1.25)
+print()
+user_secret = input(f"""     ♢ Enter a secret about yourself, {player_name.title()} > """)
+time.sleep(0.25)
+
