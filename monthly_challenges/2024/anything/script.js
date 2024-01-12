a90b0cdb62a82484e5c078ff1e0f818251cf0f66
Beingsie <ada@beingsie.com>
2024-01-11 23:58:50 -0800

    feat: add header and hero section

--- monthly_challenges/2024/anything/script.js
+++ monthly_challenges/2024/anything/script.js
@@ -1,1 +1,20 @@
 // Do it
+window.onload = function() {
+    // Get elements
+    const challengeTag = document.getElementById("challengeTag");
+    const miniDino = document.getElementById("miniDino");
+    const anythingHeading = document.getElementById("mainHeading");
+    const mainHeadingArrow = document.getElementById("mainHeadingArrow");
+
+    // Add fadeInBounce animation through class to tag
+    challengeTag.classList.add("bounceUp");
+
+    setTimeout(() => {
+        miniDino.classList.add("bounceUpDown");
+        miniDino.style.opacity = "1";
+    }, 140);
+
+    setTimeout(() => {
+        anythingHeading.classList.add("rotate-in-down-left")
+    }, 145);
+}
