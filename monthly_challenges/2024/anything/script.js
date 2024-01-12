dc2b331a5a95570e5e151907d2e1fa6361b2c747
Beingsie <ada@beingsie.com>
2024-01-12 15:32:18 -0800

    feat: update modal functionality and interactions

--- monthly_challenges/2024/anything/script.js
+++ monthly_challenges/2024/anything/script.js
@@ -8,6 +8,7 @@ window.onload = function() {
 
     // Add fadeInBounce animation through class to tag
     challengeTag.classList.add("bounceUp");
+    challengeTag.style.opacity = "1"
 
     setTimeout(() => {
         miniDino.classList.add("bounceUpDown");
@@ -17,4 +18,38 @@ window.onload = function() {
     setTimeout(() => {
         anythingHeading.classList.add("rotate-in-down-left")
     }, 145);
+}
+
+const closeBtn = document.getElementById("closeBtn");
+const modalWindow = document.getElementById("modalWindow");
+const modalContent = document.getElementById("modalContent");
+const whatLink = document.getElementById("whatLink");
+
+whatLink.addEventListener("click", openModal);
+
+function openModal(event) {
+    // Prevent from reloading upon submission/clicking button
+    event.preventDefault();
+    // Add classes
+    modalWindow.classList.add("active");
+    // 0.14 second delay before adding classes
+    setTimeout(() => {
+        modalContent.classList.add("bounceIn");
+    }, 140);
+}
+// On click run function
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+    modalContent.classList.remove("bounceIn");
+
+    setTimeout(() => {
+        modalContent.classList.add("slide-out-up");
+    }, 140);
+
+    modalContent.classList.remove("slide-out-up");
+
+    setTimeout(() => {
+        modalWindow.classList.remove("active");
+    }, 720);
 }
