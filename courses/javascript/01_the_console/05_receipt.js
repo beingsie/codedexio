f98b26a1cd6b0071fb51e6f0bb81dddc483cae1e
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-02 10:52:06 -0800

    feat: add 05_receipt.js

---
+++ courses/javascript/01_the_console/05_receipt.js
@@ -0,0 +1,13 @@
+console.log("===============================================");
+console.log("|                Apple Receipt                |");
+console.log("|---------------------------------------------|");
+console.log("|                Order Details                |");
+console.log("|---------------------------------------------|");
+console.log("| Item                   Quantity     Price($)|");
+console.log("|---------------------------------------------|");
+console.log("|                                             |");
+console.log("| 🥽  Apple Vision Pro       1           3499  |"); // Extra space is due to the use of an emojie character
+console.log("|                                             |");
+console.log("|---------------------------------------------|");
+console.log("| Total                                 3499  |");
+console.log("===============================================");
