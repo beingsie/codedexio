f5e0537833dfecb0cd7dfc06dd17e51c562a964a
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-02 10:25:09 -0800

    feat: add initials.js

---
+++ courses/javascript/01_the_console/initials.js
@@ -0,0 +1,8 @@
+// Printing initials using console log and strings
+console.log(" SSS  EEEEE");
+console.log("S   S E    ");
+console.log("S     E    ");
+console.log(" SSS  EEEEE");
+console.log("    S E    ");
+console.log("S   S E    ");
+console.log(" SSS  EEEEE");
