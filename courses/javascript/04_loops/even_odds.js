ba48f10720532177705ff76688ad30b555b7e023
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-07 11:28:20 -0800

    refactor: update even_odds.js

--- courses/javascript/04_loops/even_odds.js
+++ courses/javascript/04_loops/even_odds.js
@@ -7,3 +7,27 @@ for (let i = 0; i <5; i++) {
   // Print results
   console.log(i);
 }
+
+// Iterate from 1-5
+for (let i = 0; i < 5; i++) {
+  // If iteration is equal to 3 break from loop
+  if (i == 3) {
+    break;
+  }
+  // Print results
+  console.log(i);
+}
+
+// Iterate from 1-50
+for (i = 1; i <= 50; i++) {
+  // Skip odd numbers
+  if ((i % 2) == 1) {
+    continue;
+  }
+  // Break & log to console if 42 
+  if (i == 42) {
+    break;
+  }
+  // Print results
+  console.log(i);
+}
