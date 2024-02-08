a532c4c128869f52a51d2ccd3615276670de4a8f
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-08 09:35:50 -0800

    add: times_table.js

---
+++ courses/javascript/06_arrays/times_table.js
@@ -0,0 +1,11 @@
+// Initialize a multiple
+let multiple = 9;
+// Array of number from 0-9
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Store length value of numbers
+let numbersLength = numbers.length;
+// Loop through all numbers
+for (let i = 0; i < numbersLength; i++) {
+  // Print times table
+  console.log(multiple, "x", i, "=", multiple * i);
+}
