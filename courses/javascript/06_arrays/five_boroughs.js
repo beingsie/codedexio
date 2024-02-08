f3bb85fb1a9cfcc42a26b2b1f3fdcbe4d34345d5
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-08 08:45:27 -0800

    feat: add five_boroughs.js

---
+++ courses/javascript/06_arrays/five_boroughs.js
@@ -0,0 +1,10 @@
+// Array of NYC destinations
+const myNYCDestinations = [
+  "🗽 Manhattan - Washington Square Park",
+  "🏟️ The Bronx - Yankee Stadium",
+  "🎡 Brooklyn - Coney Island",
+  "✈️ Queens - Astoria Park",
+  "🌉 Staten Island - Historic Richmond Town"
+];
+// Print array to console
+console.log(myNYCDestinations);
