310dcf703edcbc8e777f4e940326b4a22537f6a3
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-08 09:01:09 -0800

    add: grocery_trip.js

---
+++ courses/javascript/06_arrays/grocery_trip.js
@@ -0,0 +1,13 @@
+// Array
+let groceryList = [
+  "🥛 Milk", // Indice 0
+  "🥑 Avocado", // Indice 1
+  "🥚 Eggs", // Indice 2
+  "🍞 Bread", // Indice 3
+]
+// Replace element in array
+groceryList[2] = "🧈 Butter";
+// Add element to array
+groceryList[4] = "🧼 Laundry Soap";
+// Print updated array
+console.log(groceryList);
