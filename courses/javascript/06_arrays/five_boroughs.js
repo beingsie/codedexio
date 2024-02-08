6363cc05f08d305d098f8482445809137c279434
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-08 08:49:32 -0800

    feat: add custom example to five_boroughs.js

--- courses/javascript/06_arrays/five_boroughs.js
+++ courses/javascript/06_arrays/five_boroughs.js
@@ -8,3 +8,15 @@ const myNYCDestinations = [
 ];
 // Print array to console
 console.log(myNYCDestinations);
+
+// Custom example using Manchester City FC Players
+const manCityFcPlayers = [
+  // Array of current players for MCC
+  "Number 1 - Ederson",
+  "Number 9, Erling Haaland",
+  "Number 10 - Jack Greilish",
+  "Number 11 - Jeremy Doku",
+  "Number 47 - Phil Phoden",
+];
+// Print array
+console.log(manCityFcPlayers);
