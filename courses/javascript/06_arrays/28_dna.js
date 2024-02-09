518a9a6a949dbad05300a43b7bc2628786855ddb
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-09 10:06:24 -0800

    feat: add 28_dna.js

---
+++ courses/javascript/06_arrays/28_dna.js
@@ -0,0 +1,13 @@
+let dnaPieces = ["A", "C", "G", "T"];
+let myDNA = [];
+let dnaString = "";
+
+
+for (x = 0; x < 3; x++) {
+  randomIndex = Math.floor(Math.random() * 4);
+  dnaString = dnaString + dnaPieces[randomIndex];
+}
+
+for (i = 0)
+
+console.log(myDNA);
