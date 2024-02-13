11b9d6137285db9ab4c7da0c95ef3fd376707876
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-13 09:05:29 -0800

    feat: add 29_greetings.js

---
+++ courses/javascript/07_functions/29_greetings.js
@@ -0,0 +1,33 @@
+// Modified version of the greeting function on Codedex
+
+// Function generates a random MCFC team lineup 
+function mcfcLineup() {
+  // Array containing all MCFC players
+  const mcfcPlayers = [
+    "Erling Haaland",
+    "Kevin De Bruyne",
+    "Jérémy Doku",
+    "Phil Foden",
+    "Julián Álvarez",
+    "Rodri",
+    "Matheus Nunes",
+    "Jack Grealish",
+    "Oscar Bobb",
+    "Micah Hamilton",
+    "Bernardo Silva",
+    "Ederson",
+    "Joško Gvardiol",
+    "Rico Lewis",
+    "Kyle Walker",
+    "Mateo Kovačić",
+    "Rúben Dias",
+    "Nathan Aké",
+    "Manuel Akanji",
+    "John Stones",
+    "Stefan Ortega",
+    "Mahamadou Susoho",
+    "Sergio Gómez",
+  ]
+
+  // Iterate through array
+}
