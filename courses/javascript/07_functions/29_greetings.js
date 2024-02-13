ec69d5e5440f85c571aa2b57d752315f2c7ce10e
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-13 15:36:08 -0800

    refactor: update 29_greetings.js

--- courses/javascript/07_functions/29_greetings.js
+++ courses/javascript/07_functions/29_greetings.js
@@ -1,55 +1,50 @@
 // Modified version of the greeting function on Codedex
 
-// Function generates a random MCFC team lineup 
-function mcfcLineup() {
-  // Array containing all MCFC players
-  const mcfcPlayers = [
-    "Erling Haaland",
-    "Kevin De Bruyne",
-    "Jérémy Doku",
-    "Phil Foden",
-    "Julián Álvarez",
-    "Rodri",
-    "Matheus Nunes",
-    "Jack Grealish",
-    "Oscar Bobb",
-    "Micah Hamilton",
-    "Bernardo Silva",
-    "Ederson",
-    "Joško Gvardiol",
-    "Rico Lewis",
-    "Kyle Walker",
-    "Mateo Kovačić",
-    "Rúben Dias",
-    "Nathan Aké",
-    "Manuel Akanji",
-    "John Stones",
-    "Stefan Ortega",
-    "Mahamadou Susoho",
-    "Sergio Gómez",
-  ]
-  // Number of players for a full team
-  const requiredPlayers = 11;
-  // Initialize selectedLineup array
-  const selectedLineup = [];
+// Array containing all MCFC players
+const mcfcPlayers = [
+  "Erling Haaland",
+  "Kevin De Bruyne",
+  "Jérémy Doku",
+  "Phil Foden",
+  "Julián Álvarez",
+  "Rodri",
+  "Matheus Nunes",
+  "Jack Grealish",
+  "Oscar Bobb",
+  "Micah Hamilton",
+  "Bernardo Silva",
+  "Ederson",
+  "Joško Gvardiol",
+  "Rico Lewis",
+  "Kyle Walker",
+  "Mateo Kovačić",
+  "Rúben Dias",
+  "Nathan Aké",
+  "Manuel Akanji",
+  "John Stones",
+  "Stefan Ortega",
+  "Mahamadou Susoho",
+  "Sergio Gómez",
+]
+// Number of players required to qualify
+const playersRequired = 11;
+// Initialize random number
+let randomNumber;
+// Function to generate random number
+function randomNumberGenerator() {
+  randomNumber = Math.floor(Math.random() * mcfcPlayers.length);
+}
+// Initialize selected lineup
+let selectedLineup = [];
 
-  for (i = 1; i < requiredPlayers + 1; i++) {
-    // Generated a random number
-    const randomNumber = Math.floor(Math.random() * requiredPlayers) + 1;
-    // Initialize random selected player
-    let mcfcPlayer;
-    // Iterate through mcfcPlayers array
-    for (x = 0; x < mcfcPlayers.length + 1; x++) {
-      mcfcPlayer = mcfcPlayers[x];
-    }
-    // Check for duplicates
-    if (randomNumber == mcfcPlayer)
-    // Add players to selected lineup using the random number as the index
+for (i = 0; i < playersRequired; i++) {
+  randomNumberGenerator();
+  // Check duplicate
+  if (randomNumber === mcfcPlayers[i]) {
+    console.log("Duplicate detected!");
+  } else {
     selectedLineup.push(mcfcPlayers[randomNumber]);
   }
-
-  // Print updated selectedLineup array
-  console.log(selectedLineup);
 }
 
-mcfcLineup();
+console.log(selectedLineup);
