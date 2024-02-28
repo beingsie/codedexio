13013a987a0047b1606e3d577c5dc31321731a07
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-27 16:09:36 -0800

    feat: add 24_inventory.py

---
+++ courses/python/06_lists/24_inventory.py
@@ -0,0 +1,10 @@
+# Lists
+stock1_prices = [2.52, 2.44, 2.32, 2.41, 2.51, 2.50, 2.44]
+stock2_prices = [8.36, 8.31, 8.21, 8.21, 8.25, 8.11, 8.13]
+
+# Returns the total length of a list
+print(len(stock1_prices))
+# Returns the maximum value in a list
+print(max(stock1_prices))
+# Returns the minimum value in a list
+print(min(stock2_prices))
