9db085399432ed90ac4630e8a1585d7f10325fc6
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-27 16:55:59 -0800

    feat: add 26_mixtape.py

---
+++ courses/python/06_lists/26_mixtape.py
@@ -0,0 +1,13 @@
+# 💿 Theme: Soul Food
+playlist = [
+  'Chiiild - The Best Ain\'t Happen Yet',
+  'Tame Impala - Yes I\'m Changing',
+  'Chiiild - Gone',
+  'Kyle Lux - No Roof Access',
+  'Christian Kuria - So',
+]
+
+# Iterate through the list
+for i in playlist:
+  # Print each song (i)
+  print(i)
