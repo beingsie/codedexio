ca709f46df198f79e4734b88d916ffdb52283d9f
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-27 16:04:15 -0800

    feat: add 06_lists & to_do.py

---
+++ courses/python/06_lists/to_do.py
@@ -0,0 +1,18 @@
+# Todo list
+todo = [
+  '🏦 Get quarters.',
+  '🧺 Do laundry.',
+  '🌳 Take a walk.',
+  '💈 Get a haircut.',
+  '🍵 Make some tea.',
+  '💻 Complete Lists chapter.',
+  '💖 Call mom.',
+  '📺 Watch My Hero Academia.'
+]
+
+# Using single index
+print(todo[0], todo[1])
+# Using slicing
+print(todo[2: 5])
+# Practice IndexError
+print(todo[9])
