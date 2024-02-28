44f8256560675eae79b05c71b4a76e85c9cf3b19
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-27 16:41:15 -0800

    feat: add 25_reading_list.py

---
+++ courses/python/06_lists/25_reading_list.py
@@ -0,0 +1,17 @@
+books = [
+  'Zero to One',
+  'The Lean Startup',
+  'The Mom Test',
+  'Make It Stick',
+  'Life in Code',
+]
+
+# Add item to the end of the list
+books.append('Zero to Sold')
+# Remove item based on it's value
+books.remove('Zero to One')
+# Remove item based on it's index
+books.pop(0)
+
+# Print updated list
+print(books)
