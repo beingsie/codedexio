15c9bb37cc4bd6b0ff12caa2c7b21689dbb51038
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-02-27 16:29:58 -0800

    feat: add 25_reading_list_methods.py

---
+++ courses/python/06_lists/25_reading_list_methods.py
@@ -0,0 +1,17 @@
+dna = ['AUG', 'AUC', 'UCG']
+
+# Adds an item to the end of the list
+dna.append('UAA')
+print(dna)
+
+# Adds an item to a specific index
+dna.insert(2, 'GAU')
+print(dna)
+
+# Removes an item from a list based on the value
+dna.remove('AUC')
+print(dna)
+
+# Removes the item at a particular index
+dna.pop(0)
+print(dna)
