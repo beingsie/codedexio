f54809a0af689d2b9c705533d0a1fed5496a9364
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-14 11:57:21 -0700

    feta: add 36_pokemon.js

---
+++ courses/javascript/08_objects/36_pokemon.js
@@ -0,0 +1,12 @@
+let pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25,
+}
+
+pokemon.isCaught = false;
+console.log(pokemon);
+
+pokemon.isCaught = true;
+pokemon.name = "Artex";
+console.log(pokemon);
