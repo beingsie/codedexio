c09a383a5d41860fcf9a4350f1a07c29091dbc2a
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-14 13:26:24 -0700

    refactor: add comments to 36_pokemon.js

--- courses/javascript/08_objects/36_pokemon.js
+++ courses/javascript/08_objects/36_pokemon.js
@@ -1,12 +1,18 @@
+// Create Pokemon object
 let pokemon = {
   name: "Pikachu",
   type: "Electric ⚡️",
   level: 25,
 }
 
+// Add new object property
 pokemon.isCaught = false;
+// Print updated object
 console.log(pokemon);
 
+// Update new object property
 pokemon.isCaught = true;
+// Add new object property
 pokemon.name = "Artex";
+// Print updated object
 console.log(pokemon);
