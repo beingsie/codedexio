88b965cc1be25fbe8091b23070a67b5c087e7196
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-14 11:17:13 -0700

    feat: add 35_dream-car.js

---
+++ courses/javascript/08_objects/35_dream-car.js
@@ -0,0 +1,15 @@
+// Initialize dream car object
+const car = {};
+
+// Add object properties using dot notation
+car.model = "Honda";
+car.year = 2021;
+car.color = 'White';
+car.new = false;
+
+// Control flow
+if (car.new != false) {
+  console.log("I'm looking for a new 2024 Honda 🚗");
+} else {
+  console.log("I'm looking for a used 2021 Honda 🚗");
+}
