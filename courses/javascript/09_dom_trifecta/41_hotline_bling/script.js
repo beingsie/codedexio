3538717da688f5c38f76ac530ffd3386eac85af1
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-15 08:25:28 -0700

    feat: add script.js

---
+++ courses/javascript/09_dom_trifecta/41_hotline_bling/script.js
@@ -0,0 +1,9 @@
+// Select the <img> element with the id of "drake-pic-2
+let drakePic2 = document.getElementById("drake-pic-2");
+// Change the image source
+drakePic2.src = "https://i.imgur.com/RGbh6zY.png";
+
+// Select the <h1> heading element with an id of "heading-2"
+let header = document.getElementById("heading-2");
+// Change the text of the heading variable to say "Learn By Doing"
+header.innerText = "Learn By Doing";
