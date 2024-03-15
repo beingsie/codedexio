ec94bde27afa5363b80ef2f344c7cf343758d76e
Beingsie <ada@beingsie.com>
2024-03-14 21:51:10 -0700

    feat: add 40_the_dom

---
+++ courses/javascript/09_dom_trifecta/40_the_dom/script.js
@@ -0,0 +1,17 @@
+// Open your browsers console to view in action
+
+// Select by element name
+const pTag = document.querySelector("p");
+console.log(pTag)
+
+// Select by class name
+const myClassName = document.querySelector(".class-name");
+console.log(myClassName)
+
+// Select by id name
+const idName = document.querySelector("#id-name");
+console.log(idName)
+
+// innerHTML
+const bodyInnerHtml = document.querySelector("body").innerHTML;
+console.log(bodyInnerHtml)
