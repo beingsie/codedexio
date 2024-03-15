8a062cccae3531df076bae796ef484c35335e295
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-15 09:38:27 -0700

    refactor: add bonus task to script.js

--- courses/javascript/09_dom_trifecta/41_hotline_bling/script.js
+++ courses/javascript/09_dom_trifecta/41_hotline_bling/script.js
@@ -7,3 +7,13 @@ drakePic2.src = "https://i.imgur.com/RGbh6zY.png";
 let header = document.getElementById("heading-2");
 // Change the text of the heading variable to say "Learn By Doing"
 header.innerText = "Learn By Doing";
+
+
+// ✨ Bonus ✨ - Replace both captions with something cool and witty
+
+// Select the <h1> heading element with an id of "heading-1"
+let topHeader = document.getElementById("heading-1");
+// Change the text for top header
+topHeader.innerText = "YouTube Tutorials"
+// Change the text for bottom header
+header.innerText = "Codedex Courses";
