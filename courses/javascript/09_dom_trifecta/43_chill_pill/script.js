adaca61881a45de72d98245172bf584f8cdfa2a7
Sergio Estrada <140430987+beingsie@users.noreply.github.com>
2024-03-15 16:39:39 -0700

    refactor: update script.js

--- courses/javascript/09_dom_trifecta/43_chill_pill/script.js
+++ courses/javascript/09_dom_trifecta/43_chill_pill/script.js
@@ -8,4 +8,30 @@ const quoteList = [
   "To understand the immeasurable, the mind must be extraordinarily quiet, still."
 ];
 
-const colors = ["#e81416", "#ffa500", "#faeb36", "#79c314", "#487de7", "#4b369d", "#70369d"];
+const colors = [
+  "#e81416",
+  "#ffa500",
+  "#79c314",
+  "#487de7",
+  "#4b369d",
+  "#70369d",
+];
+
+const htmlBody = document.querySelector("body");
+const wrapperDiv = document.getElementById("wrapper");
+let quoteText = document.getElementById("quote-text");
+const quoteButton = document.getElementById("quote-button");
+
+quoteButton.addEventListener("click", function () {
+  // Create a randomIndex from the quoteList array
+  let randomIndex = Math.floor(Math.random(1) * 7);
+  // Use the randomIndex to get a random quote
+  // from the quoteList array
+  quoteText.innerHTML = quoteList[randomIndex];
+  // Change the text of the quoteText variable
+  // to the value of the randomQuote variable
+  quoteText.style.color = colors[randomIndex];
+  // Change the background color of the wrapperDiv
+  htmlBody.style.backgroundColor = colors[randomIndex];
+  quoteButton.style.backgroundColor = colors[randomIndex];
+});
